cd9be3bdde2c0b34ee073b8f94bc6a498f1f4bfe
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2019-02-05 21:44:01 +0530

    Line charts

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+    const CHART = document.getElementById('lineChart');
+    var lineChart = new Chart(CHART, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                label: "My First dataset",
+                fill: false,
+                borderColor: 'teal',
+                data: [20, 10, 5, 2, 20, 30, 45],
+            }]
+        },
+    });
+});
