43ca9b9a617c83c05e87598294a196f2335b55e9
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2019-02-05 21:46:35 +0530

    Using options

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ $(document).ready(() => {
             labels: ["January", "February", "March", "April", "May", "June", "July"],
             datasets: [{
                 label: "My First dataset",
-                fill: false,
                 borderColor: 'teal',
                 data: [20, 10, 5, 2, 20, 30, 45],
             }]
         },
+        options:{
+            showLines: false,
+        }
     });
 });
